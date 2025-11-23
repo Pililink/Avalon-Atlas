5f09341883e78fd0af55254abd5ab45f57ecc2c0
qin <xiaoqin2966@gmail.com>
2025-11-23 18:46:16 +0800

    chore: 发布 v1.0.5 版本

--- atlas/version.py
+++ atlas/version.py
@@ -2,8 +2,8 @@
 
 from __future__ import annotations
 
-__version__ = "1.0.4"
-__version_info__ = (1, 0, 4)
+__version__ = "1.0.5"
+__version_info__ = (1, 0, 5)
 
 # 版本号规则 (Semantic Versioning):
 # MAJOR.MINOR.PATCH
