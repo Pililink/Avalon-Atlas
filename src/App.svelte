e611e9556387b8a156b752b8bacf658c86c835b1
qin <xiaoqin2966@gmail.com>
2026-01-10 18:29:50 +0800

    refactor: initialize Tauri project structure

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let name = "Tauri";
+</script>
+
+<main class="container">
+  <h1>Welcome to Avalon Atlas</h1>
+  
+  <p>重构进行中...</p>
+</main>
+
+<style>
+  .container {
+    margin: 0;
+    padding-top: 10vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+</style>
